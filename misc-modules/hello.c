e3330d4a957158197d47a91ed0a1864c97a73c26
Sergei <sergej.shirokov@gmail.com>
2024-12-30 21:10:33 +0200

    Personalize "hello" module

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -1,19 +1,17 @@
-/*                                                     
- * $Id: hello.c,v 1.5 2004/10/26 03:32:21 corbet Exp $ 
- */                                                    
 #include <linux/init.h>
 #include <linux/module.h>
+
 MODULE_LICENSE("Dual BSD/GPL");
 
-static int hello_init(void)
+static int __init hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world\n");
+	printk(KERN_ALERT "Hello, serges147!\n");
 	return 0;
 }
 
-static void hello_exit(void)
+static void __exit hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, serges147.\n");
 }
 
 module_init(hello_init);
